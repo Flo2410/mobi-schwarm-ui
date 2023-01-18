26e55769c1786455891d9216e6e2cbd48ed444be
Flo2410 <florian@hye.dev>
2023-01-18 19:35:31 +0100

    added some more ros msg types

--- src/types/roslib.type.ts
+++ src/types/roslib.type.ts
@@ -1,4 +1,4 @@
-import { Pose, Vector3 } from "roslib";
+import { Pose, Quaternion, Vector3 } from "roslib";
 
 export interface Header {
   seq?: number;
@@ -24,4 +24,43 @@ export interface Twist {
 export interface Path {
   header: Header;
   poses: PoseStamped[];
+}
+
+export interface PoseWithCovariance {
+  pose: Pose;
+  covariance: number;
+}
+
+export interface PoseWithCovarianceStamped {
+  header: Header;
+  pose: PoseWithCovariance;
+}
+
+export interface Imu {
+  header: Header;
+  orientation: Quaternion;
+  orientation_covariance: number;
+  angular_velocity: Vector3;
+  angular_velocity_covariance: number;
+  linear_acceleration: Vector3;
+  linear_acceleration_covariance: number;
+}
+
+export interface KeyValue {
+  key: string;
+  value: string;
+}
+
+export enum Level {
+  OK = 0,
+  WARN = 1,
+  ERROR = 2,
+  STALE = 3,
+}
+export interface DiagnosticStatus {
+  level: Level;
+  name: string;
+  message: string;
+  hardware_id: string;
+  values: KeyValue[];
 }
